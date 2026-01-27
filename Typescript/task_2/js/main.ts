d63a0361bf8d6377875b0294633176f6e8f96471
Emily Chew <emilychewsh@gmail.com>
2026-01-27 17:38:32 +1100

    task 7

--- Typescript/task_2/js/main.ts
+++ Typescript/task_2/js/main.ts
@@ -54,3 +54,30 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+// type predicate to check if employee is a Director
+function isDirector(employee: Director | Teacher): employee is Director {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (employee instanceof Director) {
+        return employee.workDirectorTasks();
+    }
+    return employee.workTeacherTasks();
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === "Math") {
+        return "Teaching Math";
+    }
+    return "Teaching History";
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
