78a56d85db86b2ac100d8bbf7a064962d8a62252
Emily Chew <emilychewsh@gmail.com>
2026-01-29 15:14:56 +1100

    task 8

---
+++ Typescript/task_3/js/crud.d.ts
@@ -0,0 +1,8 @@
+// ambient declaration file
+
+import { RowID, RowElement } from "./interface";
+
+// type declarations for each CRUD function
+export function insertRow(row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
